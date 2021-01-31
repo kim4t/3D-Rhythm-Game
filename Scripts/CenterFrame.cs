f3a09841ce4b4a810d83855446f0a59d38f26082
Taeho Kim <kim4t@cmich.edu>
2021-01-31 14:21:49 -0500

    Add files via upload

---
+++ Scripts/CenterFrame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterFrame : MonoBehaviour
+{
+
+    bool musicStart = false;
+
+    public string bgmName = "";
+
+    public bool isStart()
+    {
+        return musicStart;
+    }
+    
+    public void ResetMusic()
+    { 
+        musicStart = false;
+        Debug.Log("Reset " + musicStart);
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+        if (!musicStart)
+        {
+            if (collision.CompareTag("Note"))
+            {
+               
+                AudioManager.instance.PlayBGM(bgmName);
+
+                musicStart = true;
+            }
+        }
+
+
+    }
+
+}
